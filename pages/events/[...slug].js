5f3c5adc3ad97554b9bb7292b20c17689f14e044
Nataniel Mendes <natanielmendes@hotmail.com>
2022-04-27 17:43:17 +0800

    Final Steps

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,11 @@
 import { useRouter } from 'next/router'
+import { Fragment } from 'react'
 
 import { getFilteredEvents } from '../../dummy-data'
+import EventList from '../../components/events/event-list'
+import ResultsTitle from '../../components/events/results-title'
+import Button from '../../components/ui/button'
+import ErrorAlert from '../../components/ui/error-alert'
 
 function FilteredEventsPage() {
   const router = useRouter()
@@ -30,17 +35,29 @@ function FilteredEventsPage() {
 
   const filteredEvents = getFilteredEvents({
     year: numYear,
-    month: numMonth
+    month: numMonth,
   })
 
   if (!filteredEvents || filteredEvents.length === 0) {
-    return <p>No events found for the chosen filter!</p>
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button>Show All Events</Button>
+        </div>
+      </Fragment>
+    )
   }
 
+  const date = new Date(numYear, numMonth - 1)
+
   return (
-    <div>
-      <h1>Filtered Events</h1>
-    </div>
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
   )
 }
 
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -5,6 +5,7 @@ import { getEventById } from '../../dummy-data'
 import EventSummary from '../../components/event-detail/event-summary'
 import EventLogistics from '../../components/event-detail/event-logistics'
 import EventContent from '../../components/event-detail/event-content'
+import ErrorAlert from '../../components/ui/error-alert'
 
 function EventDetailPage() {
   const router = useRouter()
@@ -13,13 +14,22 @@ function EventDetailPage() {
   const event = getEventById(eventId)
 
   if (!event) {
-    return <p>No event found!</p>
+    return (
+      <ErrorAlert>
+        <p>No event found!</p>
+      </ErrorAlert>
+    )
   }
 
   return (
     <Fragment>
       <EventSummary title={event.title} />
-      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
